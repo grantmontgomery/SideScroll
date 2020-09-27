d6480f7e3d99dc5ede97a1a70fd9740ac78300c3
Grant <gmontgomery1994@gmail.com>
2020-09-27 14:19:50 -0700

    box setup.

--- src/components/Boxes/Boxes.tsx
+++ src/components/Boxes/Boxes.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import "./Boxes.css";
 
 export const Boxes: React.FC = () => {
   return <section className="boxesSection"></section>;
--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,15 +1,23 @@
 import * as React from "react";
+import { Boxes } from "../Boxes";
 import "./Container.css";
 
 export const Container: React.FC = () => {
+  const [state, setState] = React.useState({ color: "blue" });
+
+  const redScroll: React.MutableRefObject<any> = React.useRef();
+  const greenScroll: React.MutableRefObject<any> = React.useRef(null);
+  const blueScroll: React.MutableRefObject<any> = React.useRef(null);
+
   const observer: React.MutableRefObject<IntersectionObserver> = React.useRef(
     new IntersectionObserver(
       (entries) => {
-        console.log(entries);
-
-        if (entries.length === 1) {
-          console.log(entries[0].intersectionRatio);
-        }
+        return entries.forEach((entry) => {
+          const { target, intersectionRatio } = entry;
+          if (target === document.getElementById("red")) {
+            console.log(intersectionRatio);
+          }
+        });
       },
       {
         threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
@@ -18,32 +26,49 @@ export const Container: React.FC = () => {
     )
   );
 
-  const testRef = React.useRef(null);
-
   React.useEffect(() => {
     const newObserver = observer.current;
-    const testRefCurrent = testRef.current;
-    if (testRefCurrent) {
-      newObserver.observe(testRefCurrent);
+    const redFocus = redScroll.current;
+    const blueFocus = blueScroll.current;
+    const greenFocus = greenScroll.current;
+    if (redFocus) {
+      newObserver.observe(redFocus);
+    }
+    if (blueFocus) {
+      newObserver.observe(blueFocus);
+    }
+    if (greenFocus) {
+      newObserver.observe(greenFocus);
     }
   }, []);
 
   return (
     <div className="container">
-      <div></div>
-      <div className="secretScroll" id="secretScroll">
-        <div className="secretSection"></div>
-        <div className="secretSection"></div>
-        <div
-          ref={testRef}
-          style={{ background: "red" }}
-          className="secretSection"
-        ></div>
-        <div className="secretSection"></div>
-        <div className="secretSection"></div>
-        <div className="secretSection"></div>
-        <div className="secretSection"></div>
-        <div className="secretSection"></div>
+      <Boxes></Boxes>
+      <div className="scrollWrapper">
+        <div className="secretScroll" id="secretScroll">
+          <div className="secretSection"></div>
+          <div className="secretSection"></div>
+          <div
+            ref={redScroll}
+            style={{ border: "solid 1px red" }}
+            className="secretSection"
+            id="red"
+          ></div>
+          <div
+            ref={greenScroll}
+            style={{ border: "solid 1px green" }}
+            className="secretSection"
+          ></div>
+          <div
+            ref={blueScroll}
+            style={{ border: "solid 1px blue" }}
+            className="secretSection"
+          ></div>
+          <div className="secretSection"></div>
+          <div className="secretSection"></div>
+          <div className="secretSection"></div>
+        </div>
       </div>
     </div>
   );
