c5e221c6f8a9c34c9042ee2aafbd594b837570f9
Grant <gmontgomery1994@gmail.com>
2020-09-26 18:17:22 -0700

    lets get this started.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Container } from "./components";
+
+export const App: React.FC = () => {
+  return (
+    <div className="">
+      <Container></Container>
+    </div>
+  );
+};
---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import "./Container.css";
+
+export const Container: React.FC = () => {
+  return <div className="container"></div>;
+};
