e9785ba98009f2b5a55ce6539c2e2933d08ad90c
Grant <gmontgomery1994@gmail.com>
2020-09-28 12:18:13 -0700

    added unsplash api.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,33 @@ import * as React from "react";
 import { Container } from "./components";
 
 export const App: React.FC = () => {
+  const [state, setState] = React.useState({ pictures: [] });
+  React.useEffect(() => {
+    const key: string = `${process.env.REACT_APP_UNSPLASH}`;
+    const fetchPictures = async () => {
+      try {
+        const unsplashURL: URL = new URL(
+          "https://api.unsplash.com/search/photos/"
+        );
+
+        unsplashURL.searchParams.append("query", "berlin");
+
+        const urlString: string = unsplashURL.toString();
+
+        const pictures: Response = await fetch(urlString, {
+          headers: {
+            Authorization: key,
+          },
+        });
+        const picsJSON: JSON = await pictures.json();
+
+        console.log(picsJSON);
+      } catch (err) {
+        return err.message;
+      }
+    };
+    fetchPictures();
+  });
   return (
     <div className="">
       <Container></Container>
